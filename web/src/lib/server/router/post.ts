10c20a3f13979510aff07af8f26e798fb15a4349
AlexThatDefinetlyStudiesIT <m2208273@edu.misis.ru>
2024-10-10 16:28:29 +0000

    fix: removed unauthorized access to viewing posts

--- web/src/lib/server/router/post.ts
+++ web/src/lib/server/router/post.ts
@@ -1,10 +1,13 @@
 import { TRPCError } from "@trpc/server";
 import { protectedProcedure, router } from "./trpc";
 import { z } from "zod";
+import { Code } from "$lib/models";
 
 export default function getPostRouter() {
 	return router({
 		getUserPosts: protectedProcedure.query(async ({ ctx }) => {
+			if (ctx.session.user.id === undefined)
+				throw new TRPCError({ code: "UNAUTHORIZED" });
 			return await ctx.services.post.getUserPosts(ctx.session.user);
 		}),
 		makePost: protectedProcedure
