bed30b0e7fc8ab5bf40a4f199412c1f7e4f83098
alex <djianna12@gmail.com>
2024-10-11 00:30:02 +0000

    feat: added event model

---
+++ web/src/lib/models/event.ts
@@ -0,0 +1,12 @@
+import { z, type TypeOf } from "zod";
+import { Attachment } from "./post";
+
+export const EventPreview = z.object({
+	header: z.string(),
+	date: z.date().nullable(),
+	userId: z.number(),
+	userName: z.string(),
+	attachments: z.array(Attachment),
+	address: z.string().nullable()
+});
+export type EventPreview = TypeOf<typeof EventPreview>;
